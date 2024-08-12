ace5e1975db6cd7d8af4b1125c4a3792995dbd6c
Rafael Vesoloski <134641899+devrafaelvsk@users.noreply.github.com>
2024-08-11 21:32:18 -0300

    Atualizacoes

---
+++ funções-async/index.js
@@ -0,0 +1,19 @@
+async function asyncSubtract(a, b) {
+    return a - b
+}
+
+const sumResult = asyncSum(50, 33)
+const subtractResult = asyncSubtract(50, 33)
+
+Promise.all([sumResult, subtractResult]).then(results => {
+    console.log(results)
+})
+const numbers = [4, 9, 5, 13, 77]
+
+async function asyncSquare(x) {
+    return x * x
+}
+
+Promise.all(numbers.map(number => asyncSquare(number))).then(squares => {
+    console.log(squares)
+})
---
+++ utilizando-await/index.js
@@ -0,0 +1,33 @@
+async function asyncSum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('arguments must be of type number')
+    }
+    return a + b
+}
+
+async function execute() {
+    const sumResult = await asyncSum(50, 33)
+		console.log(sumResult)
+}
+
+execute()
+
+async function asyncSubtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return Promise.reject('arguments must be of type number')
+    }
+    return a - b
+}
+
+async function execute() {
+    try {
+        const sumResult = await asyncSum(50, 33)
+        console.log(sumResult)
+        const subtractResult = await asyncSubtract(50, null)
+        console.log(subtractResult)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+execute()
